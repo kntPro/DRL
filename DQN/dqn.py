46464efa96b14979479bc9bb9b6ffb471ec9fe15
kento <kentoyamamoto26@gmail.com>
2023-02-16 06:47:55 +0000

    add dqn_model

---
+++ DQN/dqn.py
@@ -0,0 +1,16 @@
+import warnings
+from typing import Any, Dict, List, Optional, Tuple, Type, TypeVar, Union
+import numpy as np
+import tensorflow as tf
+from gym import spaces
+
+SelfDQN = TypeVar("SelfDQN", bound="DQN")
+
+
+class DQN():
+
+    def __init__(self) -> None:
+        pass
+
+    def set_model(self) -> None:
+        pass

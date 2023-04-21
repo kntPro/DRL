781ed2c4609a512cb24e2a8e59ae2a6f49698f20
kento <kentoyamamoto26@gmail.com>
2023-04-21 15:09:14 +0900

    none

--- DQN/test.py
+++ DQN/test.py
@@ -44,3 +44,38 @@ print(f"Predicted class: {y_pred}")
 #tensor([[ 0.0820, -0.0153, -0.1121,  0.0854, -0.0653,  0.1364,  0.0372, -0.0080,
 #         -0.0267,  0.0210]], device='cuda:0', grad_fn=<AddmmBackward0>)
 #Predicted class: tensor([5], device='cuda:0')
+
+
+
+class FuncNeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.one=nn.Linear(28*28, 512)
+        self.two=nn.ReLU()
+        self.three=nn.Linear(512, 512)
+        self.four=nn.ReLU()
+        self.five=nn.Linear(512, 10)
+        self.six=nn.Linear(512,5)
+
+    def forward(self,x):
+        x=self.flatten(x)
+        x=self.one(x) 
+        x=self.two(x)
+        x=self.three(x)
+        x=self.four(x)
+        y=self.five(x)
+        z=self.six(x)
+
+        return y,z
+
+FModel = FuncNeuralNetwork().to(device)
+print(FModel)
+X = torch.rand(1,28,28,device=device)
+logits1,logits2= FModel(X)
+pred_probab1 = nn.Softmax(dim=1)(logits1)
+pred_probab2 = nn.Softmax(dim=1)(logits2)
+print(f"logits1: {logits1}")
+print(f"probab1: {pred_probab1}")
+print(f"logits2: {logits2}")
+print(f"probab2: {pred_probab2}")
